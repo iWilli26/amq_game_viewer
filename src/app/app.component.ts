7952caadfe59bf3ad8cdcf9767132eb706ee8e55
iWilli26 <willi.nguyen@hotmail.fr>
2024-04-06 18:52:08 +0200

    readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Amq } from 'src/model/amq';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AMQ';
+  jsonData: Amq = {
+    roomName: '',
+    startTime: '',
+    songs: [],
+  };
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    console.log(file);
+    if (file.type !== 'application/json') {
+      console.error('Error: File type must be JSON');
+      return;
+    }
+    const reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = () => {
+      try {
+        this.jsonData = JSON.parse(reader.result as string);
+        console.log(this.jsonData.songs);
+      } catch (error) {
+        console.error('Error parsing JSON file:', error);
+      }
+    };
+  }
 }
---
+++ src/model/amq.ts
@@ -0,0 +1,48 @@
+export type Amq = {
+  roomName: string;
+  startTime: string;
+  songs: Song[];
+};
+
+export type Song = {
+  songNumber: number;
+  songInfo: SongInfo;
+  animeTags: string[];
+  answer: string;
+  correctGuess: number;
+  wrongGuess: boolean;
+  correctCount: number;
+  wrongCount: number;
+  startPoint: number;
+  videoLength: number;
+};
+
+export type SongInfo = {
+  animeNames: Names;
+  altAnimeNames: string[];
+  altAnimeNamesAnswers: string[];
+  artist: string;
+  songName: string;
+  type: number;
+  typeNumber: number;
+  annId: number;
+  animeScore: number;
+  animeType: string;
+  vintage: string;
+  animeDifficulty: number;
+  siteIds: SiteIds;
+  animeTags: string[];
+  animeGenre: string[];
+};
+
+export type Names = {
+  english: string;
+  romaji: string;
+};
+
+export type SiteIds = {
+  aniListId: number;
+  malId: number;
+  annId: number;
+  kitsuId: number;
+};
